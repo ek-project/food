ed026f087d360058cc05e78e80dd20e79840a240
subham146 <subhamjeet2003@gmail.com>
2024-02-28 22:31:52 +0530

    signup page completed

--- resources/js/ls_script.js
+++ resources/js/ls_script.js
@@ -1,5 +1,5 @@
 $('.small').click(function() {
-    var icon = $(".small");
+    var icon = $(this);
     var password = $('#pwd2');
 
     password.attr('type', password.attr('type') === 'password' ? 'text' : 'password');
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
   function checkPasswords() {
       // Check if the passwords match
-      if ((password1.val() === password2.val()) && (password1.val().length >= 8 && password1. val().length <= 16)) {
+      if ((password1.val() === password2.val()) && (password1.val().length >= 8 && password1.val().length <= 16)) {
           // If they match, enable the button
           submitBtn.prop("disabled", false);
       } else if ((password1.val().length == 0) && (password2.val().length == 0)){
@@ -37,4 +37,31 @@ $(document).ready(function() {
       }
   }
 
+});
+$("#submit-btn").click(function(e) {
+    e.preventDefault();
+    $("#success-message").text('').hide()
+    $("#error-message").text('').hide()
+    if ($("#name").val() && $("#email").val() && $("#pwd1").val() && $("#pwd2").val()) {
+        if ($("#pwd1").val() === $("#pwd2").val()) {
+            $.ajax({
+                url: "resources/php/signup.php",
+                type: "POST",
+                data: $(".contact-form").serialize(),
+                success: function(response) {
+                    if (response === "Record added successfully!") {
+                        $("#success-message").text(response).show();
+                        $(".contact-form")[0].reset();
+                    } else {
+                        $("#error-message").text(response).show();
+                        $(".contact-form")[0].reset();
+                    }
+                }
+            });
+        } else {
+            $("#error-message").text("Error: Passwords do not match.");
+        }
+    } else {
+        $("#error-message").text("Error: All fields are required.");
+    }
 });
